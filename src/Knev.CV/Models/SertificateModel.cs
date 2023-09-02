500f51053c6725030820ee8020e393e73163f869
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2023-09-02 23:05:49 +0300

    ADD: route with data diplomas

---
+++ src/Knev.CV/Models/SertificateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Knev.CV.Models
+{
+    public class SertificateModel
+    {
+        public string Key { get; set; } = default!;
+        public string Title { get; set; } = default!;
+        public string Uri { get; set; } = default!;
+        public string Description { get; set; } = default!;
+    }
+}
